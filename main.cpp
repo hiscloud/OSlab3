ee89d23e10047a018685da47bdfcfab3d21b7dbd
hiscloud <49000237+hiscloud@users.noreply.github.com>
2020-05-17 13:05:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,52 +3,58 @@
 
 using namespace std;
 
-void bubbleSort(double* arr)
+
+// The number of elements (n) should be provided as command line argument, while the
+//number of threads (M) should be read from the console.
+void bubbleSort(double* arr, int size)
 {
-cout<<sizeof(*arr)<<endl;
-  for (int i=0; i< sizeof(*arr);i++)
+  
+  for (int i=0; i< size;i++)
   {
-    for (int j=0; j< sizeof(*arr)-1; j++)
+    for (int j=0; j< size-1; j++)
     {
       if (arr[j]>arr[j+1])
       {
         swap(arr[j],arr[j+1]);
+        //arr[j]=arr[j+1];
       }
     }
     
   }
-  
 }
 
-void insertionSort(double* arr)
+void insertionSort(double* arr, int size)
 {
-  for( int i=0;i<sizeof(arr);i++)
+  
+  for( int i=0;i<size;i++)
   {
     int k=i;
-    if(k>0)
-      while(arr[k]<arr[k-1])
+    while(k>0)
+      if (arr[k]<arr[k-1])
       {
         swap(arr[k],arr[k-1]);
         k--;
-      }
+      }else
+        break;
   }
 }
 int main()
 {
-  double a[3]={105,110.2,2354.2};
+  double a[3]={1050,110.2,2354.2};
   double* arr;
+
   arr=a;
-  cout<<sizeof(a)<<endl;
-  cout<<arr[0]<<endl;
-  cout<<arr[1]<<endl;
-  cout<<arr[2]<<endl;
-  
-  bubbleSort(arr);
   
   cout<<arr[0]<<endl;
   cout<<arr[1]<<endl;
   cout<<arr[2]<<endl;
- insertionSort(arr);
+  
+  bubbleSort(arr,3);
+  
+  cout<<arr[0]<<endl;
+  cout<<arr[1]<<endl;
+  cout<<arr[2]<<endl;
+ insertionSort(arr,3);
   
   cout<<arr[0]<<endl;
   cout<<arr[1]<<endl;
