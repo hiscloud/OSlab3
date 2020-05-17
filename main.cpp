4408f2ce8214b7489f663a7ad65dd7ecf68cf8d8
hiscloud <49000237+hiscloud@users.noreply.github.com>
2020-05-16 23:23:57 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,59 @@
 #include<iostream>
+#include<array>
+
 using namespace std;
 
+void bubbleSort(double* arr)
+{
+cout<<sizeof(*arr)<<endl;
+  for (int i=0; i< sizeof(*arr);i++)
+  {
+    for (int j=0; j< sizeof(*arr)-1; j++)
+    {
+      if (arr[j]>arr[j+1])
+      {
+        swap(arr[j],arr[j+1]);
+      }
+    }
+    
+  }
+  
+}
 
-
-
+void insertionSort(double* arr)
+{
+  for( int i=0;i<sizeof(arr);i++)
+  {
+    int k=i;
+    if(k>0)
+      while(arr[k]<arr[k-1])
+      {
+        swap(arr[k],arr[k-1]);
+        k--;
+      }
+  }
+}
 int main()
 {
-
+  double a[3]={105,110.2,2354.2};
+  double* arr;
+  arr=a;
+  cout<<sizeof(a)<<endl;
+  cout<<arr[0]<<endl;
+  cout<<arr[1]<<endl;
+  cout<<arr[2]<<endl;
+  
+  bubbleSort(arr);
+  
+  cout<<arr[0]<<endl;
+  cout<<arr[1]<<endl;
+  cout<<arr[2]<<endl;
+ insertionSort(arr);
+  
+  cout<<arr[0]<<endl;
+  cout<<arr[1]<<endl;
+  cout<<arr[2]<<endl;
+  
   
   return 0;
 }
